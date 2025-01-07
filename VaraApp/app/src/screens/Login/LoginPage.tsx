34e68328dda570c43d3a465057a70eb9474a1f4f
“TRENCH13” <118077906+TRENCH13@users.noreply.github.com>
2025-01-07 12:41:13 -0600

    refactor LoginPage: se añadió el bloqueo por tiempo después de 4 intentos fallidos.

--- VaraApp/app/src/screens/Login/LoginPage.tsx
+++ VaraApp/app/src/screens/Login/LoginPage.tsx
@@ -1,12 +1,13 @@
 import { useRouter } from "expo-router";
 import {View, Alert, Pressable, Text, TouchableWithoutFeedback, Keyboard} from "react-native";
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import LoginForm from "varaapplib/components/LoginForm/LoginForm";
 import {Login} from "../../services/AuthServices";
 import {LoginResponse, LoginViewModel} from "../../services/AuthServiceInterfaces";
 import { LinearGradient } from "expo-linear-gradient";
 import LoginPageStyle from "./LoginPage.style";
 import useAuthStore from "../../hooks/useStore";
+import {set} from "react-hook-form";
 
 const LoginPage: React.FC = () => {
     const router = useRouter();
@@ -14,9 +15,25 @@ const LoginPage: React.FC = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [loading, setLoading] = useState(false);
+    //Estados que manejan el número de intentos y el tiempo de bloqueo que se tiene
+    const [failedAttempts, setFailedAttempts] = useState(0);
+    const [isBlocked, setIsBlocked] = useState(false);
+    const [blockTime, setBlockTime] = useState(0);
 
     const { setToken } = useAuthStore();
 
+    useEffect(() => {
+        if (blockTime > 0) {
+            const timer = setInterval(() => {
+                setBlockTime(prev => prev - 1);
+            }, 1000);
+
+            return () => clearInterval(timer);
+        } else if (blockTime === 0 && isBlocked) {
+            setIsBlocked(false);
+            setFailedAttempts(0);
+        }
+    }, [blockTime, isBlocked]);
 
     const handleRegistroCientifico = () =>{
         router.push({
@@ -25,6 +42,16 @@ const LoginPage: React.FC = () => {
     };
 
     const handleLogin = async () => {
+        if (isBlocked) {
+            const minutosBloqueo = Math.floor(blockTime / 60); //Se redondea al primer digito
+            const segundosBloqueo = blockTime % 60;
+            Alert.alert(
+                "Bloqueo temporal activo",
+                `Has sido bloqueado temporalmente. Intenta nuevamente en ${minutosBloqueo} minutos y ${segundosBloqueo} segundos.`
+            );
+            return;
+        }
+
         setLoading(true);
         try {
             const loginData: LoginViewModel = {
@@ -46,10 +73,21 @@ const LoginPage: React.FC = () => {
             });
 
         }catch (error){
-            Alert.alert(
-                "Error al iniciar sesión",
-                "Verifique sus credenciales"
-            );
+            setFailedAttempts(prev => prev + 1);
+
+            if (failedAttempts + 1 >= 4) {
+                setIsBlocked(true);
+                setBlockTime(60 * 4); // Bloqueo en segundos
+                Alert.alert(
+                    "Cuenta bloqueada temporalmente",
+                    "Has excedido los intentos permitidos. Intenta de nuevo en 4 minutos."
+                );
+            } else {
+                Alert.alert(
+                    "Error al iniciar sesión",
+                    `Verifique sus credenciales o comuníquese con soporte. Intentos restantes: ${4 - (failedAttempts + 1)}`
+                );
+            }
             setLoading(false);
         } finally {
             setLoading(false)
